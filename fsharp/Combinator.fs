1cb8f15fafce302f21153e6fc528dd25e23d02ac
Tony Vo <ttrung.vo@gmail.com>
2021-12-06 16:45:55 -0700

    r extract createValidationResultFromMultipleValidationResults

--- fsharp/Combinator.fs
+++ fsharp/Combinator.fs
@@ -8,10 +8,13 @@ module Combinator
             |> List.collect errorMessageFromInvalidResult
         InvalidResult errorMessages
 
+    let createValidationResultFromMultipleValidationResults isValid validationResult1 validationResult2 =
+        if (isValid) then createValidResult else aggregateErrors validationResult1 validationResult2
+        
     let andCombine leftPredicate rightPredicate =
         let isValid = isValid leftPredicate && isValid rightPredicate
-        if (isValid) then createValidResult else aggregateErrors leftPredicate rightPredicate
-
+        createValidationResultFromMultipleValidationResults isValid leftPredicate rightPredicate
+    
     let orCombine leftPredicate rightPredicate =
         let isValid = isValid leftPredicate || isValid rightPredicate
-        if (isValid) then createValidResult else aggregateErrors leftPredicate rightPredicate
+        createValidationResultFromMultipleValidationResults isValid leftPredicate rightPredicate
