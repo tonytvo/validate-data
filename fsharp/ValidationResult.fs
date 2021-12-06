beff8e5144561ccbc9a35d5f9da7aa0f5f4c32ef
Tony Vo <ttrung.vo@gmail.com>
2021-12-06 16:49:53 -0700

    r rename to createInvalidResult

--- fsharp/ValidationResult.fs
+++ fsharp/ValidationResult.fs
@@ -4,12 +4,12 @@ module ValidationResult
         | InvalidResult of string list
     
     let createValidResult = ValidResult
-    let createSingleInvalidResult (errorMessage: string) = InvalidResult [errorMessage] 
+    let createInvalidResult (errorMessage: string) = InvalidResult [errorMessage] 
     
     let createValidationResultOnPredicate predicate errorMessage =
         if predicate
         then createValidResult
-        else createSingleInvalidResult errorMessage
+        else createInvalidResult errorMessage
     
     let isValid result =
         match result with
