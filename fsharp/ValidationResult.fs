96881a77f1c864e76061ae749e1653cf38c927cd
Tony Vo <ttrung.vo@gmail.com>
2021-12-06 13:36:47 -0700

    R!! add multipleInvalidResults domain concept

--- fsharp/ValidationResult.fs
+++ fsharp/ValidationResult.fs
@@ -2,6 +2,7 @@ module ValidationResult
     type T =
         | ValidResult of bool
         | InvalidResult of string
+        | MultipleInvalidResults of string list
     
     let create (result:bool) = ValidResult result
     let createWithErrorMessage (errorMessage: string) = InvalidResult errorMessage 
